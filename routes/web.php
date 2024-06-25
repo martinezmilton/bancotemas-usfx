e79ca64dd61be4ef1a144e74fcf233ec2f64f6e2
martinezmilton <martinez.rodriguez.miltonangel@usfx.bo>
2024-06-25 15:24:47 -0400

    ultimaver

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\SearchController;
 use App\Http\Controllers\EstudianteController;
 
 Route::get('/', function () {
-    return view('/search');
+    return view('/temas');
 });
 
 Route::get('/dashboard', function () {
