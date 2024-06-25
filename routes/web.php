643fca9fb89191890ecfefec922dc0a2441b7f78
martinezmilton <martinez.rodriguez.miltonangel@usfx.bo>
2024-06-25 14:44:17 -0400

    last

--- routes/web.php
+++ routes/web.php
@@ -9,7 +9,7 @@ use App\Http\Controllers\SearchController;
 use App\Http\Controllers\EstudianteController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('auth/login');
 });
 
 Route::get('/dashboard', function () {
